60b1a9db334a06c07e0fbf75061ed99c000e7e32
chenweigao <297859260@qq.com>
2018-12-19 17:43:11 +0800

    添加新的界面以及本地睡眠的统计

--- pages/health/health.js
+++ pages/health/health.js
@@ -1,4 +1,5 @@
 // pages/health/health.js
+var app = getApp()
 wx.cloud.init({
   env: 'tencent-c-9d0b3f'
 })
@@ -23,16 +24,19 @@ Page({
     stopTime: null,
     totalSleepTime: null,
     cloudSleepData: null,
-    startTimeString: null
+    startTimeString: null,
+    sleepFormated: null,
+    toView: 'red',
+    scrollTop: 100
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+ 
 
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -86,52 +90,87 @@ Page({
     console.log(e)
     var date = new Date(e._relatedInfo.anchorTapTime)
     console.log(date.toString())
-    this.setData({ startDay: date.toLocaleDateString(), startTime: date.getTime(), startTimeString: date.toLocaleString() })
+    var y = date.getFullYear()
+    var m = (date.getMonth() + 1).toString().padStart(2, '0')
+    var d = date.getDate().toString().padStart(2, '0')
+    var hh = date.getHours().toString().padStart(2, '0')
+    var mm = date.getMinutes().toString().padStart(2, '0')
+    var ss = date.getSeconds().toString().padStart(2, '0')
+    var formattedTime = y + "-" + m + '-'+ d  + " " + hh + ":" + mm
+    console.log(formattedTime)
+    this.setData({
+      startDay: date.toLocaleDateString(),
+      startTime: date.getTime(),
+      startTimeString: formattedTime
+    })
     // console.log(date.toLocaleDateString())
 
   },
   stop: function (e) {
-    console.log(e)
+    // console.log(e)
     var date = new Date(e._relatedInfo.anchorTapTime)
-    this.setData({ stopDay: date.toLocaleDateString(), stopTime: date.getTime() })
+    this.setData({
+      stopDay: date.toLocaleDateString(),
+      stopTime: date.getTime()
+    })
     console.log(this.data.stopTime)
     if (this.data.startTime == null || this.data.stopTime == null) {
       console.log("no time...")
-    }
-    else {
+    } else {
       var timeShift = (this.data.stopTime - this.data.startTime)
       var days = parseInt(timeShift / (1000 * 60 * 60 * 24))
       var hours = parseInt((timeShift % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
       var minutes = parseInt((timeShift % (1000 * 60 * 60)) / (1000 * 60))
       var seconds = (timeShift % (1000 * 60)) / 1000
-      var sleepTime = hours + "(h)" + minutes + "(m)" + seconds + "(s)"
-      var sleepMinutes = 60 * hours + minutes
-      this.setData({ totalSleepTime: sleepTime, totalSleepMinutes: sleepMinutes })
-      console.log(this.data.totalSleepMinutes)
+      var sleepTime = hours.toString().padStart(2, '0') + "(h)" + minutes.toString().padStart(2, '0') + "(m)" + seconds.toString().padStart(2, '0') + "(s)"
+      var sleepMinutes = timeShift / 1000
+      this.setData({
+        totalSleepTime: sleepTime,
+        totalSleepMinutes: sleepMinutes
+      })
+      db.collection('sleep').add({
+        data: {
+          startDay: this.data.startDay,
+          startTimeString: this.data.startTimeString,
+          totalSleepTime: this.data.totalSleepTime,
+          totalSleepMinutes: this.data.totalSleepMinutes
+        }
+      }).then(res => {
+        // console.log(res)
+      })
+      db.collection('sleep').where({
+        startDay: this.data.startDay
+      }).
+        get().then(res => {
+          console.log(res.data)
+          var sum = 0
+          for (let i = 0; i < res.data.length; i++) {
+            sum += res.data[i].totalSleepMinutes
+          }
+          var minuteTime = 0
+          var hourTime = 0
+          var secondTime = parseInt(sum)
+          if (secondTime > 60) {
+            minuteTime = parseInt(secondTime / 60);
+            secondTime = parseInt(secondTime % 60); 
+            if (minuteTime > 60) {
+              hourTime = parseInt(minuteTime / 60);
+              minuteTime = parseInt(minuteTime % 60);
+            }
+          }
+          var sleepComputedMinutes = (sum / 60).toFixed(2)
+          var sleepComputedSeconds = sum % 60
+          var sleepFormated = hourTime + "小时" + minuteTime + "分钟" + secondTime + "秒"
+          this.setData({
+            cloudSleepData: res.data,
+            sleepFormated: sleepFormated
+          })
+        })
+      // console.log(this.data.cloudSleepData)
+      // for (let i = 0; i < this.data.cloudSleepData)
+      // console.log(this.data.totalSleepMinutes)
     }
-  },
-
-  cloudUpload: function () {
-    db.collection('sleep').add({
-      data: {
-        startDay: this.data.startDay,
-        startTimeString: this.data.startTimeString,
-        totalSleepTime: this.data.totalSleepTime,
-        totalSleepMinutes: this.data.totalSleepMinutes
-      }
-    }).then(res => {
-      console.log(res)
-    })
-  },
-
-  getDatabase: function() {
-    db.collection('sleep').where({
-      startDay: this.data.startDay
-    }).
-    get().then(res => {
-      console.log(res.data)
-      this.setData({ cloudSleepData: res.data})
-    })
   }
+  
 
 })
